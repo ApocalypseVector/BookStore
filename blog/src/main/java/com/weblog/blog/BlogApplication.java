97da2f38f8f863d0e9803be5f98f2ede876ae261
vector <vector@qq.com>
2020-07-08 19:07:13 +0800

    IDEA 18.14 尝试提交上GitHub服务器

--- blog/src/main/java/com/weblog/blog/BlogApplication.java
+++ blog/src/main/java/com/weblog/blog/BlogApplication.java
@@ -5,7 +5,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BlogApplication {
-    public static void main(String[] args) {
-        System.out.println("大家好才是真的好");
-        SpringApplication.run(BlogApplication.class, args); }
+    public static void main(String[] args) { SpringApplication.run(BlogApplication.class, args); }
 }
